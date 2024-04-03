0ebdbb0652ccb07a2a15613453e48d306f55c111
SardorQobilov <sardorqobilov555@gmail.com>
2024-04-04 00:24:03 +0500

    project finished

--- js/script.js
+++ js/script.js
@@ -10,3 +10,79 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+// const sl = document.querySelectorAll(".swiper-pagination-bullet");
+// const hoverBox = document.querySelector(".hover-box p");
+// sl.forEach((item) => {
+//   item.addEventListener("mouseover", () => {
+//     hoverBox.classList.add("salom");
+//   });
+// });
+// var paginationBullets = document.querySelectorAll(".swiper-pagination-bullet");
+
+// paginationBullets.forEach(function (bullet, index) {
+//   var textToShow = bullet.nextElementSibling.innerText;
+
+//   bullet.addEventListener("mouseover", function () {
+//     showText(textToShow);
+//     moveTextDown();
+//   });
+
+//   bullet.addEventListener("mouseout", function () {
+//     moveTextUp();
+//   });
+// });
+
+// // Matn chiqarish funksiyasi
+// function showText(text) {
+//   var textElement = document.getElementById("pagination-text");
+//   if (textElement) {
+//     // Agar element mavjud bo'lsa
+//     textElement.innerText = text;
+//   }
+// }
+
+// // Matnni pastga surish funksiyasi
+// function moveTextDown() {
+//   var textElement = document.getElementById("pagination-text");
+//   if (textElement) {
+//     // Agar element mavjud bo'lsa
+//     textElement.style.top = "44%";
+//   }
+// }
+
+// // Matnni yuqoriga qaytarish funksiyasi
+// function moveTextUp() {
+//   var textElement = document.getElementById("pagination-text");
+//   if (textElement) {
+//     // Agar element mavjud bo'lsa
+//     textElement.style.top = "40%";
+//   }
+// }
+
+// menu
+const closeBtn = document.querySelector(".close-btn");
+const openMenuBtn = document.querySelector(".open-menu__btn");
+const menu = document.querySelector(".menu");
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("scale-x-1");
+  menu.classList.add("scale-x-0");
+});
+openMenuBtn.addEventListener("click", () => {
+  menu.classList.add("scale-x-1");
+  menu.classList.remove("scale-x-0");
+});
+
+// select language
+const openSelect = document.querySelector(".open-select");
+const selectBox = document.querySelector(".select-box");
+const option = document.querySelectorAll(".option");
+const selectedLanguage = document.querySelector(".selected-language");
+
+openSelect.addEventListener("click", () => {
+  selectBox.classList.toggle("scale-y-0");
+  option.forEach((item) => {
+    item.addEventListener("click", () => {
+      selectedLanguage.textContent = item.textContent;
+    });
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ module.exports = {
       backgroundImage: {
         "slider-img": "'./images/slider-img-1.png'",
         "intro-bg": "url(../images/intro-bg.png)",
+        "nav-left-bg": "url(../images/nav-left-bg.png)",
+        "nav-right-bg": "url(../images/nav-right-bg.png)",
+        "nav-bg": "url(../images/nav-bg.png)",
         "intro-gradient": "linear-gradient(180deg, rgba(0, 0, 0, 0) 11.49%, rgba(0, 0, 0, 0.63) 77.39%)"
       },
       colors: {
         orange: "#D98910",
         blue: "#106BD9",
+        black: "#242424",
         gray: {
           100: "#818181",
           200: "#959596",
